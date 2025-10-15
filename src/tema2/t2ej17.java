524037138f23d5b90c4703bb70d5f605a6850155
IvanGPzzz <ivagarpaz@alu.edu.gva.es>
2025-10-15 14:11:52 +0200

    ejscond2

--- src/tema2/t2ej17.java
+++ src/tema2/t2ej17.java
@@ -16,16 +16,16 @@ public class t2ej17 {
                 double superficie = base * base;
                 double perimetro = base * 4;
                 System.out.println("Superficie cuadrado = " + superficie + "Perímetro cuadrado = " + perimetro);
-                return;
+                break;
             case 1:
                 superficie = base * altura;
                 perimetro = (base + altura) * 2;
                 System.out.println("Superficie rectángulo = " + superficie + "Perímetro rectángulo = " + perimetro);
-                return;
+                break;
             case 2:
                 superficie = (double)(base * altura) / 2;
                 System.out.println("Superficie triángulo = " + superficie);
-                return;
+                break;
             default:
                 System.out.println("Caso erróneo");
         }

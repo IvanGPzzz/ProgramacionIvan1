b3953f1eb617c5c2cce15e5f384913f1a1146fb0
IvanGPzzz <ivagarpaz@alu.edu.gva.es>
2025-12-15 00:40:16 +0100

    strings incompleto

--- src/tema3/T3EJ3AHORCADO.java
+++ src/tema3/T3EJ3AHORCADO.java
@@ -3,6 +3,6 @@ package tema3;
 public class T3EJ3AHORCADO {
     /* no he sido capaz de plantear por mí mismo
     este ejercicio ni el del lingo, tampoco con
-    ayuda de la ia  porque usa métodos que no
-    hemos dado aún */
+    ayuda de la ia porque usa métodos que no
+    conozco */
 }
